515a10f9e23a009c37cebad1fb4a8a77ddbdaa28
Micha yotam Vardy <micha.vardy@tufin.com>
2022-08-13 22:11:43 +0300

    lexer

---
+++ md_ast/example1.md
@@ -0,0 +1,41 @@
+# example
+code block 1
+```
+print('hello world')
+```
+
+code block 2
+```
+import pandas as pd
+pd.DataFrame()
+```
+
+definition
+: this is a definition
+
+## flat list
+- a
+- b
+- c
+- d
+
+## heiarchy list
+- 1
+  - 1.1
+  - 1.2
+    - 1.2.1
+      - 1.2.1.1
+    - 1.2.2
+    - 1.2.3
+  - 1.3
+  - 1.4
+- 2
+- 3
+  - 3.1
+  - 3.2
+  - 3.3
+
+## picture
+![title](picture1.svg)
+![title](picture2.svg)
+![title](picture3.svg)
--- md_ast/lexer.py
+++ md_ast/lexer.py
@@ -1,11 +1,8 @@
 import re
+from pathlib import Path
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Optional, List, Generator
-
-
-class TokenType(Enum):
-    NEWLINE = auto()
 
 
 @dataclass
@@ -14,15 +11,49 @@ class Token:
     value: str
     groups: Optional[List[str]] = None
 
-
 @dataclass
 class LexingRule:
     token_type: TokenType
     pattern: Optional[str] = None
 
+# noinspection PyArgumentList
+class TokenType(Enum):
+    TEXT_INLINE = 1
+    NEWLINE = auto()
+    TITLE_1 = auto()
+    TITLE_2 = auto()
+    TITLE_3 = auto()
+    TITLE_4 = auto()
+    TITLE_5 = auto()
+    TITLE_6 = auto()
+    CODE_BLOCK = auto()
+    DEFINITION = auto()
+    LIST_1 = auto()
+    LIST_2 = auto()
+    LIST_3 = auto()
+    LIST_4 = auto()
+    LIST_5 = auto()
+    LIST_6 = auto()
+    PICTURE = auto()
+
 
 lexing_rules = [
     LexingRule(token_type=TokenType.NEWLINE, pattern="\n"),
+    LexingRule(token_type=TokenType.TITLE_1, pattern=r"(^\#\s)([a-zA-Z]+)"),
+    LexingRule(token_type=TokenType.TITLE_2, pattern=r"(^\#\#\s)([a-zA-Z]+)"),
+    LexingRule(token_type=TokenType.TITLE_3, pattern=r"(^\#\#\#\s)([a-zA-Z]+)"),
+    LexingRule(token_type=TokenType.TITLE_4, pattern=r"(^\#\#\#\#\s)([a-zA-Z]+)"),
+    LexingRule(token_type=TokenType.TITLE_5, pattern=r"(^\#\#\#\#\#\s)([a-zA-Z]+)"),
+    LexingRule(token_type=TokenType.TITLE_6, pattern=r"(^\#\#\#\#\#\#\s)([a-zA-Z]+)"),
+    LexingRule(token_type=TokenType.CODE_BLOCK, pattern=r"\`\`\`"),
+    LexingRule(token_type=TokenType.DEFINITION, pattern=r"(\:\s)(.*)"),
+    LexingRule(token_type=TokenType.LIST_1, pattern=r"(^\-\s)(.*)"),
+    LexingRule(token_type=TokenType.LIST_2, pattern=r"(^\s{3}\-\s)(.*)"),
+    LexingRule(token_type=TokenType.LIST_3, pattern=r"(^\s{5}\-\s)(.*)"),
+    LexingRule(token_type=TokenType.LIST_4, pattern=r"(^\s{7}\-\s)(.*)"),
+    LexingRule(token_type=TokenType.LIST_5, pattern=r"(^\s{9}\-\s)(.*)"),
+    LexingRule(token_type=TokenType.LIST_6, pattern=r"(^\s{11}\-\s)(.*)"),
+    LexingRule(token_type=TokenType.PICTURE, pattern=r"\!(\[.*\])(\(.*\))")
 ]
 
 
@@ -55,6 +86,7 @@ def lex(input_text: str) -> Generator[Token, None, None]:
 
 
 if __name__ == "__main__":
-    sample_text = "something\nsomethingelse"
+    sample_text = Path('./md_ast/md_ast/example1.md').read_text()
     tokens = lex(sample_text)
+
 
